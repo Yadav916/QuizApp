b8b5a7de6cb289e883e2c3ee78fc0d4352a3a14a
YadavK <yadav.ks@infospica.com>
2025-11-25 23:38:44 +0530

    added packages properly

--- src/main/java/com/example/quiz/controller/QuestionController.java
+++ src/main/java/com/example/quiz/controller/QuestionController.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.example.quiz;
+package com.example.quiz.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/quiz/service/QuestionService.java
+++ src/main/java/com/example/quiz/service/QuestionService.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.example.quiz;
+package com.example.quiz.service;
 
 /**
  *
